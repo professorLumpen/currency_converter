5975219be354c557d1c86d758cbfba7effa78695
professorLumpen <jeanpaulvsratyi@gmail.com>
2025-06-19 12:19:12 +0300

    Added config and pre-commit

---
+++ app/core/config.py
@@ -0,0 +1,20 @@
+from dotenv import find_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=find_dotenv())
+
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    CURRENCY_API_KEY: str
+
+    @property
+    def ASYNC_DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+settings = Settings()
