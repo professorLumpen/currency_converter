f40b3a60e2de6c38b52dc336845849a6fd1b5f5b
professorLumpen <jeanpaulvsratyi@gmail.com>
2025-06-19 12:46:58 +0300

    Added database engine and alemmbic

---
+++ app/db/database.py
@@ -0,0 +1,20 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+from app.core.config import settings
+
+
+async_database_engine = create_async_engine(settings.ASYNC_DATABASE_URL)
+async_database_session = async_sessionmaker(bind=async_database_engine, class_=AsyncSession)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_database_session() as session:
+        yield session
