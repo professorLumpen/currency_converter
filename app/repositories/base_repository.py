0dda2e2d44a71f24b7ef101d73ac823c9a3d16db
professorLumpen <jeanpaulvsratyi@gmail.com>
2025-06-20 11:40:06 +0300

    Added database repository

---
+++ app/repositories/base_repository.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+
+from sqlalchemy import and_, select
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+
+class AbstractRepository(ABC):
+    @abstractmethod
+    async def find_one(self, data: dict):
+        pass
+
+    @abstractmethod
+    async def find_all(self):
+        pass
+
+    @abstractmethod
+    async def add_one(self, data: dict):
+        pass
+
+    @abstractmethod
+    async def delete_one(self, data: dict):
+        pass
+
+    @abstractmethod
+    async def update_one(self, filter_data: dict, update_data: dict):
+        pass
+
+
+class Repository(AbstractRepository):
+    model = None
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def find_one(self, data: dict):
+        conditions = [getattr(self.model, key) == val for key, val in data.items()]
+        query = select(self.model).where(and_(*conditions))
+        result = await self.session.execute(query)
+        return result.scalar_one()
+
+    async def find_all(self):
+        result = await self.session.execute(select(self.model))
+        return result.scalars().all()
+
+    async def add_one(self, data: dict):
+        new_obj = self.model(**data)
+        self.session.add(new_obj)
+        await self.session.commit()
+        await self.session.refresh(new_obj)
+        return new_obj
+
+    async def delete_one(self, data: dict):
+        obj = await self.find_one(data)
+        if obj:
+            await self.session.delete(obj)
+            await self.session.commit()
+            return True
+        return False
+
+    async def update_one(self, filter_data: dict, update_data: dict):
+        obj = await self.find_one(filter_data)
+        if obj:
+            for key, value in update_data.items():
+                setattr(obj, key, value)
+            await self.session.commit()
+            await self.session.refresh(obj)
+            return obj
+        return False
